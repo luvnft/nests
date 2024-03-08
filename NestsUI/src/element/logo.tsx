c4ba263864d719ada5cdefb3d96c445f8682b354
The Wizard of Hahz <info@luvnft.com>
2024-03-08 06:10:54 -0500

    audio

--- NestsUI/src/element/logo.tsx
+++ NestsUI/src/element/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link to="/">
       <div className="w-12 h-12 rounded-2xl bg-foreground flex items-center justify-center text-2xl font-bold select-none">
-        N
+       🗣️
       </div>
     </Link>
   );
